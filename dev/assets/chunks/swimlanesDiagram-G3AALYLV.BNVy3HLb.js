123953ef9a89fae91336657fae4a6d00a56fa17c
Documenter.jl <documenter@juliadocs.github.io>
2026-07-20 20:28:11 +0000

    build based on e6fa2dc

---
+++ dev/assets/chunks/swimlanesDiagram-G3AALYLV.BNVy3HLb.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-23GEKE2U.DsEKLM0d.js";import{_ as a}from"../app.D4p_kOIx.js";import"./chunk-5VM5RSS4.BxRXOEvJ.js";import"./chunk-XXDRQBXY.DT2UwsV_.js";import"./chunk-VR4S4FIN.BnaWhnMG.js";import"./chunk-32BRIVSS.CD1NyPat.js";import"./channel.Ckb1a-JU.js";import"./framework.B08aFN1B.js";import"./theme.CxcVHG7h.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
