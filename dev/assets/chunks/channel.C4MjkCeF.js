2f6216eb890f5b0df7776871b895375a66423ade
Documenter.jl <documenter@juliadocs.github.io>
2026-08-14 19:22:28 +0000

    build based on 5cfd19d

---
+++ dev/assets/chunks/channel.C4MjkCeF.js
@@ -0,0 +1,1 @@
+import{U as a,C as n}from"../app.B89Y_8sX.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
--- dev/assets/chunks/channel.DpZ1Jq9U.js
+++
@@ -1,1 +0,0 @@
-import{U as a,C as n}from"../app.pc7_tfPU.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
