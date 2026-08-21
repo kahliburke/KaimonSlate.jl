3f50ffe950aee0e088e46c0f3df7e251a672e1b1
Documenter.jl <documenter@juliadocs.github.io>
2026-08-21 10:05:51 +0000

    build based on b7fdfa3

---
+++ dev/assets/chunks/chunk-2Q5K7J3B.BkyAh7QK.js
@@ -0,0 +1,1 @@
+import{_ as s}from"../app.Rq24Bb5d.js";var t,e=(t=class{constructor(i){this.init=i,this.records=this.init()}reset(){this.records=this.init()}},s(t,"ImperativeState"),t);export{e as I};
--- dev/assets/chunks/swimlanesDiagram-ULZ7WXOC.B5Qjh3ed.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-UKHOOZJN.C4HS-_Yu.js";import{_ as a}from"../app.db1TYZ2_.js";import"./chunk-5VM5RSS4.BgQcUdm1.js";import"./chunk-XXDRQBXY.B0jwvs8g.js";import"./chunk-KBJHAD2P.2pUzmojo.js";import"./chunk-2GRJ4B5K.CJtlB7Tr.js";import"./channel.CZvLi9Bz.js";import"./framework.BXm-Zw-h.js";import"./theme.Cm4qDiyL.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
